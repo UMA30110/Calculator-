c32ee433b6a7ce0408ddcab0d4feebb3ae4fc50a
UMA30110 <umaranimekala692@gmail.com>
2025-05-05 15:29:51 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,24 @@
+function appendValue(val) {
+    document.getElementById('display').value +=val;
+
+}
+function clearDisplay() {
+    document.getElementById('display').value= "";
+ 
+}
+function deleteLast(){
+    let display=document.getElementById("display");
+    display.value = display.value.slice(0,-1);
+}
+function calculateResult(){
+    try{
+        const result = eval(document.getElementById("display").value);
+        document.getElementById("display").value=result;
+
+    }
+    catch{
+        document.getElementById("display").value='error';
+
+    }
+}
+    
